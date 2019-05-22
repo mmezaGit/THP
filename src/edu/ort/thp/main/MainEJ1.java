0eca6e54a877faa7064cf1a059240301160ff0b5
mmezaGit <44303353+mmezaGit@users.noreply.github.com>
2019-05-22 20:33:04 -0300

    Add files via upload

---
+++ src/edu/ort/thp/main/MainEJ1.java
@@ -0,0 +1,17 @@
+package edu.ort.thp.main;
+import edu.ort.thp.clases.*;
+
+
+public class MainEJ1 {
+	
+	public static void main(String[] args) {
+
+		Persona p1 = new Persona("Diego","Diaz","Roosevelt 3313");
+		Persona p2 = new Persona("Pablo","Gomez","Virrey del pino 2284");
+		
+		System.out.println(p1.toString());
+		System.out.println(p2.toString());
+		
+	}
+
+}
